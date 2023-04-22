32942b15df61bcf61c8ec0c6a5921fbec9413c1f
Anuj Vadecha <anujvadecha@Anujs-MacBook-Pro.local>
2023-04-22 11:55:31 -0700

    $.

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -166,3 +166,11 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+REST_FRAMEWORK = {
+	'DEFAULT_AUTHENTICATION_CLASSES': [
+		'rest_framework_simplejwt.authentication.JWTAuthentication',
+	],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+}
