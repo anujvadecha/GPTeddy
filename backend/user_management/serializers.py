e3ec3cd2f9c53b248eaac148772af35ee0927839
Anuj Vadecha <anujvadecha@Anujs-MacBook-Pro.local>
2023-04-22 16:36:47 -0700

    $.

--- backend/user_management/serializers.py
+++ backend/user_management/serializers.py
@@ -1,9 +1,12 @@
+import json
+
 from rest_framework import serializers
 
 from user_management.models import Prompts
 
 
 class PromptSerializer(serializers.ModelSerializer):
+    subjects = serializers.SerializerMethodField(read_only=True)
     # filters = FilterSerializer(many=True,)
     #
     # def create(self, validated_data):
@@ -12,6 +15,8 @@ class PromptSerializer(serializers.ModelSerializer):
     #     for filter in filters:
     #         Filters.objects.create(prompt = prompt, **filter)
     #     return prompt
+    def get_subjects(self, obj):
+        return json.loads(obj.subjects)
 
     class Meta:
         model = Prompts
